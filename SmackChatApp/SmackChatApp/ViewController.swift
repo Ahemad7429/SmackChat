e80d54d355ff726f5587eb24c479299f83148222
AhemadAbbas Vagh <>
2019-05-19 03:50:39 +0530

    Initial Project Setup

---
+++ SmackChatApp/SmackChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmackChatApp
+//
+//  Created by AhemadAbbas Vagh on 19/05/19.
+//  Copyright © 2019 AhemadAbbas Vagh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
